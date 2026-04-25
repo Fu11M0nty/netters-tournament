25ec2bc1f186f4a079dfd07d7eb86494b5af2ab1
Bav Patel <bav.j.patel@gmail.com>
2026-04-25 20:53:08 +0100

    swapped to html2canvas-pro, a maintained drop-in fork that supports oklch/lab/lch plus a few other modern CSS features.

--- src/components/PrintButton.tsx
+++ src/components/PrintButton.tsx
@@ -44,7 +44,7 @@ export default function PrintButton({
 
     try {
       const [{ default: html2canvas }, { default: jsPDF }] = await Promise.all([
-        import('html2canvas'),
+        import('html2canvas-pro'),
         import('jspdf'),
       ])
 
